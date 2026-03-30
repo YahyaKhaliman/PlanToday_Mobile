e08d320653f2d37ef0fd3f1a62686ba2fec2f4a2
Yahyakhaliman <yahyaindrayana35@gmail.com>
2026-03-30 11:24:05 +0700

    fix: nonaktifkan baseURL untuk SERVER EDP

--- services/api.tsx
+++ services/api.tsx
@@ -3,7 +3,8 @@ import axios from 'axios';
 const API = axios.create({
   // baseURL: 'http://10.0.2.2:3001/api', // EMULATOR
   // baseURL: 'http://localhost:3001/api', // LOKAL
-  baseURL: 'http://192.168.1.87:3001/api', // SERVER EDP
+  // baseURL: 'http://192.168.1.87:3001/api', // SERVER EDP
+  baseURL: 'http://103.94.238.252:3005/api', // SERVER VPS
   timeout: 30000,
 });
 
