6ba620f836362265b612abf5e35435844881721e
Yahyakhaliman <yahyaindrayana35@gmail.com>
2026-04-22 13:53:12 +0700

    feat: Tambah konfigurasi Jest untuk pengujian

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  testEnvironment: 'node',
 };
